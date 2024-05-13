2838932c1fccb1a7301a627af12cacad4ba33b60
vluca997 <vluca997@gmail.com>
2024-05-13 22:24:22 +0200

    Add CRUD  Room

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,11 +1,29 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
 
 const router = express.Router();
+
+//CRUD ROUTES----------------------------------
+//CREATE
+router.post("/:hotelid", verifyAdmin,createRoom);
+
+//UPDATE
+router.put("/:id",verifyAdmin,updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid",verifyAdmin,deleteRoom);
+
+//GET-SINGLE
+router.get("/:id",getRoom);
+
+//GET-ALL
+router.get("/",getRooms);
 
 
 
 export default router;
-
 
 
 
